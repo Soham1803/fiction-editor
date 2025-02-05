81e4e309cc9ec756c769e1c10214dfa27377009b
Soham <mastpanchalsoham@gmail.com>
2025-02-05 21:05:27 +0530

    feat: Creates basic structure for the editor

---
+++ src/app/app_components/Navbar.tsx
@@ -0,0 +1,6 @@
+
+export default function Navbar() {
+    return (
+        <div className="absolute top-0 left-0 flex flex-row items-center h-11 w-full bg-slate-400">Navbar</div>
+    )
+}
---
+++ src/app/app_components/RightSidebar.tsx
@@ -0,0 +1,5 @@
+export default function RightSidebar() {
+    return (
+        <div className="mt-11  h-[calc(100vh-2.75rem)] w-[20%] bg-slate-200">right-sidebar</div>
+    )
+}
