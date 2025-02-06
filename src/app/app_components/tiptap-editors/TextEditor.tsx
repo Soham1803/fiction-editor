fdaecd6b0d11404f115f3a6c78032146a2bd8443
Soham1803 <sohamdesu@gmail.com>
2025-02-06 20:46:45 +0530

    feat: Adds character and word count UI

--- src/app/app_components/tiptap-editors/TextEditor.tsx
+++ src/app/app_components/tiptap-editors/TextEditor.tsx
@@ -5,6 +5,7 @@ import StarterKit from '@tiptap/starter-kit';
 import TextAlign from '@tiptap/extension-text-align';
 import Typography from '@tiptap/extension-typography'
 import { Selection } from '@tiptap/pm/state';
+import CharacterCount from '@tiptap/extension-character-count';
 
 export default function TextEditor() {
 
@@ -25,6 +26,7 @@ export default function TextEditor() {
         extensions: [
             StarterKit,
             Typography,
+            CharacterCount,
             TextAlign.configure({
                 types: ['heading', 'paragraph'],
             }),
@@ -61,15 +63,21 @@ export default function TextEditor() {
     return (
         <div className="relative mt-11 h-[calc(100vh-2.75rem)] w-[60%] bg-slate-400 p-1">
             <EditorContent className='h-full overflow-y-auto' editor={editor} />
+            <div className='absolute top-3 right-10 text-sm'>
+                <p>Characters: {editor.storage.characterCount.characters()}</p>
+                <p>Words: {editor.storage.characterCount.words()}</p>
+            </div>
             {editor && <BubbleMenu editor={editor} tippyOptions={{duration: 100}} >
-                <button onClick={() => editor.chain().focus().toggleBold().run()} className="p-2">B</button>
-                <button onClick={() => editor.chain().focus().toggleItalic().run()} className="p-2">I</button>
-                <button onClick={() => editor.chain().focus().toggleStrike().run()} className="p-2">S</button>
-                <button onClick={() => editor.chain().focus().setTextAlign('left').run()} className="p-2">Left</button>
-                <button onClick={() => editor.chain().focus().setTextAlign('center').run()} className="p-2">Center</button>
-                <button onClick={() => editor.chain().focus().setTextAlign('right').run()} className="p-2">Right</button>
-                <button onClick={() => editor.chain().focus().setTextAlign('justify').run()} className="p-2">Justify</button>
-                <button onClick={handleReplace} className="p-2">Replace</button>
+                <div className='flex flex-row'>
+                 <button onClick={() => editor.chain().focus().toggleBold().run()} className="p-2">B</button>
+                 <button onClick={() => editor.chain().focus().toggleItalic().run()} className="p-2">I</button>
+                 <button onClick={() => editor.chain().focus().toggleStrike().run()} className="p-2">S</button>
+                 <button onClick={() => editor.chain().focus().setTextAlign('left').run()} className="p-2">Left</button>
+                 <button onClick={() => editor.chain().focus().setTextAlign('center').run()} className="p-2">Center</button>
+                 <button onClick={() => editor.chain().focus().setTextAlign('right').run()} className="p-2">Right</button>
+                 <button onClick={() => editor.chain().focus().setTextAlign('justify').run()} className="p-2">Justify</button>
+                 <button onClick={handleReplace} className="p-2">Replace</button>
+                </div>
             </BubbleMenu>}
         </div>
     )
